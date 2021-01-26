4acaa9d8d50a307cf449e96f8b271fa275d37f4a
Alison Lubar <aslubar@gmail.com>
2021-01-25 20:54:52 -0500

    added footer and it is functional

--- profileapp/src/App.js
+++ profileapp/src/App.js
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AboutMe from './pages/AboutMe';
-import Contact from "./pages/Contact";
-import Nav from "./components/Nav"
+import Contact from './pages/Contact';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route exact path="/posts/:id" component={Detail} />
             <Route component={NoMatch} /> */}
           </Switch>
+          <Footer />
         </div>
       </Router>
     </div>
---
+++ profileapp/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+
+        <footer className="card" id="footer">
+            <div className="card-body" >
+                <p>
+                    Copyright &#169; 2021 Alison Lubar
+      </p>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer;
